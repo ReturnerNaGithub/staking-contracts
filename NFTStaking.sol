621bb4a44301359cab3044bc5b59963cbe29228b
ReturnerNaGithub <78732470+ReturnerNaGithub@users.noreply.github.com>
2024-07-20 21:09:25 +0200

    Update NFTStaking.sol
    sprememba imena

--- NFTStaking.sol
+++ NFTStaking.sol
@@ -22,7 +22,7 @@ contract NFTStaking is Ownable, IERC721Receiver {
 
   // reference to the Block NFT contract
   Collection nft;
-  N2DRewards token;
+  MineRewards token;
 
   // maps tokenId to stake
   mapping(uint256 => Stake) public vault; 
