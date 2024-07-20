c6b6e5d3ba4ba3481e88d8a1f1cc714251163ded
ReturnerNaGithub <78732470+ReturnerNaGithub@users.noreply.github.com>
2024-07-20 21:02:35 +0200

    Update NFTStakingV3-BringYourOwnNFTCollection.sol
    poskus

--- NFTStakingV3-BringYourOwnNFTCollection.sol
+++ NFTStakingV3-BringYourOwnNFTCollection.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.4;
 
-import "https://github.com/net2devcrypto/n2dstaking/N2DRewards.sol";
+import "https://github.com/ReturnerNaGithub/staking-contracts/blob/main/nftstakingV2.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
