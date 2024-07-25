dd915c7055cfcf71e47c6b18453f383a7fecbbc7
ReturnerNaGithub <78732470+ReturnerNaGithub@users.noreply.github.com>
2024-07-25 19:08:29 +0200

    Update Collection.sol
    string

--- Collection.sol
+++ Collection.sol
@@ -3,6 +3,7 @@
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 pragma solidity ^0.8.20;
 
